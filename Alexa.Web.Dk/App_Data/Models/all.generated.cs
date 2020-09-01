19c0cbc8929fd49db18bf8c3b07a435de4083aec
Casper Staehr <32480275+Lillefyren@users.noreply.github.com>
2020-09-01 14:51:16 +0200

    Added low-res pictures from the archive

--- Alexa.Web.Dk/App_Data/Models/all.generated.cs
+++ Alexa.Web.Dk/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "ae0518774ee10041")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
